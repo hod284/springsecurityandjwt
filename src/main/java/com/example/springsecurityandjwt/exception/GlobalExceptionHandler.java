73d646f2b4ffb503dd86258df3135e5ebc646f2d
hod284 <61046278+hod284@users.noreply.github.com>
2026-02-10 17:45:28 +0900

    12
    2121

--- src/main/java/com/example/springsecurityandjwt/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/springsecurityandjwt/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
